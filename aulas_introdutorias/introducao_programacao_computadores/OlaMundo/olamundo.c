da570831849770c7d8d239c27ce2120d74fc39c1
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-07-29 23:23:33 -0300

    Criando o primeiro programa em C, compilando e executando

--- aulas_introdutorias/introducao_programacao_computadores/OlaMundo/olamundo.c
+++ aulas_introdutorias/introducao_programacao_computadores/OlaMundo/olamundo.c
@@ -1,9 +1,14 @@
-// Declaração de bibliotecas
+// Inclusão da biblioteca: #include inclui a biblioteca padrão de entrada e saída, 
+// necessária para usar a função printf
 #include <stdio.h>
 
-// Função principal do programa
+// Função principal: int main() é onde o programa começa a execução. 
+// Toda aplicação em C começa pela função main
 int main(){
+    // Saída de dados: A linha printf("Olá, Mundo!\n"); imprime a mensagem "Olá, Mundo!" 
+    // na tela. O \n adiciona uma nova linha ao final da mensagem
     printf("Olá, Mundo!\n");
 
+    // Fim do programa: return 0; indica que o programa terminou com sucesso
     return 0;
 }
