9135c0d931335c14928c6297afa130223c46d541
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-02 11:11:47 -0300

    Modificadores de tipos de dados (short, int, long, float, double...)

---
+++ aulas_introdutorias/introducao_programacao_computadores/03_nivel_mestre/n3_nc1/n3_nc1_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    double numeroPreciso = 3.141592653589793;
+    long double numeroMuitoPreciso = 3.141592653589793238463;
+    // double numeroMuitoPreciso = 3.141592653589793238463;
+
+    printf("Número preciso (double): %.15f\n", numeroPreciso);
+    printf("Número muito preciso (long double): %.21Lf\n", numeroMuitoPreciso);
+    // printf("Número muito preciso (long double): %21lf\n", numeroMuitoPreciso);
+
+    return 0;
+}
