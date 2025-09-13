c925f8e17523233df922d2135933a1b633450347
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-09-13 11:35:53 -0300

    Trabalhando com a automatização de uma TV utilizando a linguagem Java

---
+++ algoritmos_estrutura_dados/aula01/UtilizarTV.java
@@ -0,0 +1,30 @@
+public class UtilizarTV {
+    public static void main(String[] args){
+        Televisao t1 = new Televisao();
+        Televisao t2 = new Televisao();
+        t1.defineMarca("Samsung");
+        t1.defineCanal(3);
+        t1.local("SALA");
+        t1.ligar();
+        t1.defineCanal(3);
+        t1.aumentarVolume();
+        t1.aumentarVolume();
+
+        // t2.defineMarca("LG");
+        // t2.local("COZINHA");
+        // t2.ligar();
+        // t2.aumentarVolume();
+        t1.aumentarVolume();
+
+        // t2.aumentarVolume();
+        // t2.aumentarVolume();
+        // t2.aumentarVolume();
+        // t2.aumentarVolume();
+        // t2.aumentarVolume();
+        // t2.aumentarVolume();
+
+        t1.desligar();
+        // t2.desligar();
+
+    }
+}
